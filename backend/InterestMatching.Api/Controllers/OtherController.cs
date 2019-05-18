30a3b68619fa74347751860184b4d22000b78c1c
Pavel Pushkarev <paulpv@skbkontur.ru>
2019-05-19 01:47:21 +0500

    Table Processor

--- backend/InterestMatching.Api/Controllers/OtherController.cs
+++ backend/InterestMatching.Api/Controllers/OtherController.cs
@@ -1,21 +1,51 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using InterestMatching.Engine;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InterestMatching.Api.Controllers
 {
-    [Route("api/Other")]
+    [Route("api/SetUser")]
     [ApiController]
-    public class OtherController : Controller
+    public class SetUserController : Controller
     {
         [HttpGet]
-        public string WeatherForecasts()
+        public string SetUser()
         {
-            User first;
-            using(var connector = new BDConnector())
-                first = connector.Get<User>("UserIfo").First();
+            var first = new User()
+            {
+                Age = 12,
+                Description = "Test User",
+                Email = "test@mail.test",
+                Interests = new[] {1, 2, 3, 4},
+                Name = "John",
+                SurName = "Dow",
+                UserStats = new UserStats()
+                {
+                    GoodMood = 2,
+                    NonPunctuality = 3,
+                    SelfCentered = 9
+                }
+            };
+            
 
-            return $"{first.Name} + {first.Age}";
+            UserTable.Add("test@mail.test", first);
+
+            return "OK";
+        }
+    }
+
+    [Route("api/GetUser")]
+    [ApiController]
+    public class GetUserController : Controller
+    {
+        [HttpGet]
+        public string GetUser()
+        {
+            var first = UserTable.Get("test@mail.test");
+            if (first == null) return "User does not exist";
+
+            return $"{first.Name} + {first.UserStats.GoodMood} + {first.UserStats.Sociability} + {first.Age}";
         }
     }
 }
