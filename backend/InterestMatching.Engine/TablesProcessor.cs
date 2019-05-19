6774f064b3720ab99298b3400cc3cf1b8a3aed56
Pavel Pushkarev <paulpv@skbkontur.ru>
2019-05-19 12:28:46 +0500

    cleanup

--- backend/InterestMatching.Engine/TablesProcessor.cs
+++ backend/InterestMatching.Engine/TablesProcessor.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
 
 namespace InterestMatching.Engine
@@ -12,8 +10,10 @@ namespace InterestMatching.Engine
         public static User Get(string mail)
         {
             using (var connector = new BDConnector())
+            {
                 return connector.GetListFromJsons<User>("UserIfo", "UserInfo", $"ID = \"{mail}\"").FirstOrDefault();
             }
+        }
 
         public static bool Contatins(string mail)
         {
@@ -23,33 +23,36 @@ namespace InterestMatching.Engine
         public static List<User> GetAll()
         {
             using (var connector = new BDConnector())
+            {
                 return connector.GetListFromJsons<User>("UserIfo", "UserInfo");
             }
+        }
 
         public static void Add(params User[] users)
         {
             using (var connector = new BDConnector())
+            {
                 foreach (var user in users)
                     connector.Add("UserIfo", Tuple.Create("ID", user.Email),
                         Tuple.Create("UserInfo", JsonConvert.SerializeObject(user)));
             }
+        }
 
         public static void Update(string mail, User user)
         {
             using (var connector = new BDConnector())
+            {
                 connector.Update("UserIfo", "UserInfo", JsonConvert.SerializeObject(user), $"ID = \"{mail}\"");
             }
         }
+    }
 
     public static class MeetingsTable
     {
         public static List<Meeting> GetAll()
         {
             var meetings = new List<Meeting>();
-            foreach (var id in GetIDs())
-            {
-                meetings.Add(GetById(id));
-    }
+            foreach (var id in GetIDs()) meetings.Add(GetById(id));
 
             return meetings;
         }
@@ -57,7 +60,9 @@ namespace InterestMatching.Engine
         public static List<int> GetIDs()
         {
             using (var connector = new BDConnector())
+            {
                 return connector.GetIntList("Meetings", "ID");
+            }
         }
 
         public static Meeting GetById(int id)
@@ -65,7 +70,7 @@ namespace InterestMatching.Engine
             using (var connector = new BDConnector())
             {
                 var condition = $"ID = {id}";
-                var meeting = new Meeting()
+                var meeting = new Meeting
                 {
                     FirstUserId = connector.GetFirstString("Meetings", "ID", condition),
                     SecondUserId = connector.GetFirstString("Meetings", "FirstUserId", condition),
@@ -81,27 +86,31 @@ namespace InterestMatching.Engine
         public static bool Contatins(int id)
         {
             using (var connector = new BDConnector())
+            {
                 return connector.GetFirstString("Meetings", "FirstUserID", $"ID = {id}") != null
-                    &&connector.GetFirstString("Meetings", "SecondUserID", $"ID = {id}") != null;
+                       && connector.GetFirstString("Meetings", "SecondUserID", $"ID = {id}") != null;
+            }
         }
 
         private static bool Contatins(Meeting meeting)
         {
             return meeting.FirstUserId != null
-                       && meeting.SecondUserId != null;
+                   && meeting.SecondUserId != null;
         }
 
         public static void Add(params Meeting[] meetings)
         {
             using (var connector = new BDConnector())
+            {
                 foreach (var meeting in meetings)
-                    connector.Add("Meetings", 
-                        Tuple.Create("ID", meeting.Id.ToString()), 
+                    connector.Add("Meetings",
+                        Tuple.Create("ID", meeting.Id.ToString()),
                         Tuple.Create("FirstUserId", meeting.FirstUserId),
                         Tuple.Create("SecondUserId", meeting.SecondUserId),
                         Tuple.Create("Date", meeting.Date.ToString("yyyy-MM-dd HH:mm:ss")),
                         Tuple.Create("FirstUserSetRate", meeting.FirstUserSetRate ? "1" : "0"),
                         Tuple.Create("SecondUserSetRate", meeting.SecondUserSetRate ? "1" : "0"));
+            }
         }
     }
 }
